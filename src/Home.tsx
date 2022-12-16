d95d7d3473874df098f9b7d5deb9d98a6c753fb1
santosfk <santosguilhermefk@gmail.com>
2022-12-15 21:32:25 -0300

    add textsArray in home

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Button from './components/Button'
+import TextsArray from './components/TextsArray'
 
 const Home = () => {
   return (
     <View>
       <Button>Botao</Button>
-   
+      <TextsArray texts={['1','2','3']} />
           </View>
   )
 }
