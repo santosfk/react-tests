0783dbb1d8fc05eff288c310ec55af689c0b7a84
santosfk <santosguilhermefk@gmail.com>
2022-12-15 21:33:11 -0300

    test button and text array

--- src/__test__/Home.test.tsx
+++ src/__test__/Home.test.tsx
@@ -1,15 +1,22 @@
 import { render } from "@testing-library/react-native";
 import Button from "../components/Button";
 import Home from '../Home'
-describe("star rating component", () => {
-  describe("show icon", () => {
-    it("show the home icon", () => {
-      const { getByTestId } = render(<Button>Botao</Button>);
-      expect(getByTestId("description")).toBeTruthy();
-    });
-  });
+import TextsArray from '../components/TextsArray'
+describe("Button", () => {
   it("renderer button component", () => {
     const { debug, getByText } = render(<Button>Botao</Button>);
     expect(getByText("Botao")).toBeTruthy();
   });
 });
+describe("show icon", () => {
+  it("show the home icon", () => {
+    const { getByTestId } = render(<Button>Botao</Button>);
+    expect(getByTestId("description")).toBeTruthy();
+  });
+});
+describe('textArray',()=>{
+  it('have text prop',()=>{
+const {getAllByText} = render(<TextsArray texts={['1','2','3']}/>)
+expect(getAllByText('Texto',{exact:false}).length).toBe(3)
+  })
+})
